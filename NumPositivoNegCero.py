016c9e96a5f705c2c7cb7ce28b67580910922434
Alejandroo_5 <av0622032025@gmail.com>
2025-03-05 16:46:21 -0600

    numeros positivosNegativos y cero

---
+++ NumPositivoNegCero.py
@@ -0,0 +1,7 @@
+numero = float(input("escriba un numero: "))
+if numero > 0:
+    print(f"{numero} es positivo")
+elif numero < 0:
+    print(f"{numero} es negativo")
+else:
+    print(f"el numero es cero")
