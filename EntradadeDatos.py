ec6b4e0d7a8061a34942e32b3e81176dce5f7632
Alejandroo_5 <av0622032025@gmail.com>
2025-03-05 16:42:29 -0600

    Entrada de datos

---
+++ EntradadeDatos.py
@@ -0,0 +1,11 @@
+mensaje = input("ingrese un texto: ")
+print(mensaje)
+
+valor = str(mensaje) * int(input("ingrese un numero: "))
+print(valor)
+
+a1 = input("ingrese un numero: ")
+a2 = input("ingrese otro numero: ")
+suma = int(a1) + int(a2)
+print(suma)
+print(a1 + a2)
